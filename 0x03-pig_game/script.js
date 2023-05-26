a19eecfd36d46af5a3cbf116354a77bdd8961520
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-05-26 11:48:17 +0100

    updated the game description

--- 0x03-pig_game/script.js
+++ 0x03-pig_game/script.js
@@ -1,4 +1,19 @@
 "use strict";
+// 1. This is a two-player game.
+// 2. There are three buttons: "New Game," "Roll Dice," and "Hold."
+// 3. To start the game, click on the "New Game" button.
+// 4. Player One begins their turn by clicking on the "Roll Dice" button, which generates a random number between one and six.
+// 5. If the generated number is one, it becomes Player Two's turn.
+// 6. Player One can choose to hold the generated number by clicking the "Hold" button, or they can roll the dice again to get a new random number.
+// 7. If the generated number is not one, it is added to Player One's current score.
+// 8. If the generated number is one, Player One loses their accumulated score, and it becomes Player Two's turn.
+// 9. Whenever a player decides to hold the generated number by clicking the "Hold" button, it is added to their total score.
+// 10.The game continues with alternating turns between the players.
+// 11. Each player's current score is updated by adding the newly generated numbers when they decide to hold.
+// 12. The game ends when one of the players reaches a score of 100 or more.
+// 13. The player who reaches a score of 100 or more wins the game.
+// 14. After the game concludes, players have the option to start a new game and play again.
+
 // Selecting elements
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
@@ -74,7 +89,7 @@ btnHold.addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //  2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //  Finish the game;
       playing = false;
       diceEl.classList.add("hidden");
@@ -85,8 +100,6 @@ btnHold.addEventListener("click", function () {
         .querySelector(`.player--${activePlayer}`)
         .classList.remove("player--active");
     } else {
-      //  Finish the game
-
       // Switch to next player
       switchPlayer();
     }
