0e33bf92e092264fa2ddacc7eca25a7cc1ec04f0
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-05-27 14:00:49 +0100

    Red Green Blue color slider

---
+++ 0x06-RGB_Color_Slider/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 1 Red, Green and Blue color slider
+
+function colors() {
+  var red = document.getElementById("red").value;
+  var green = document.getElementById("green").value;
+  var blue = document.getElementById("blue").value;
+  document.body.style.backgroundColor =
+    "rgb(" + red + "," + green + "," + blue + ")";
+  document.getElementById("output").innerHTML =
+    "rgb(" + red + "," + green + "," + blue + ")";
+}
