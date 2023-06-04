00b264f8272e9395ff386e881272a790334b5508
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-06-04 15:35:33 +0100

    Restructured the html

--- 0x11-weather_app/script.js
+++ 0x11-weather_app/script.js
@@ -1,11 +1,26 @@
+"use strict";
+
 let weather = {
   apiKey: "fd14c8eb94b816316764ed5c6bb7368b",
-  fetchWeather: () => {
+  fetchWeather: function () {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=France&unit=metric&limit=5&appid=fd14c8eb94b816316764ed5c6bb7368b"
+      "https://api.openweathermap.org/data/2.5/weather?q=France&units=metric&limit=5&appid=fd14c8eb94b816316764ed5c6bb7368b"
     )
       .then((responce) => responce.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
   },
-  displayWeather: () => {},
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, temp, speed, humidity, description);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    // document.querySelector(".icon").src = icon;
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+  },
 };
