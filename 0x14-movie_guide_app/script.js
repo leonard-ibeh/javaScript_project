2b4b8d22afb6f5ff806047d46b2af9689ad75ed0
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-06-27 01:49:20 +0100

    Movie Guide Project

---
+++ 0x14-movie_guide_app/script.js
@@ -0,0 +1,38 @@
+let movieNameRef = document.getElementById("movie-name");
+
+let searchBtn = document.getElementById("search-btn");
+
+let result = document.getElementById("result");
+
+// ====Function to Fetch data from API =====
+
+let getMovie = () => {
+  let movieName = movieNameRef.value;
+  let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${process.env.key}`;
+  // if Input field is empty
+  if (movieName.length <= 0) {
+    result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
+  }
+
+  //   if input is Not empty
+  else {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        console.log(data.Poster);
+        console.log(data.Title);
+        console.log(data.imdbRating);
+        console.log(data.imdbRating);
+        console.log(data.Rated);
+        console.log(data.Year);
+        console.log(data.Runtime);
+        console.log(data.Genre);
+        console.log(data.Plot);
+        console.log(data.Actors);
+      });
+  }
+};
+
+//if input
+window.addEventListener("load", getMovie);
